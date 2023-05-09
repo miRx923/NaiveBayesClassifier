8f739a0d64ec28bab61ab531ed3d4450bf7344d0
miRx923 <94078303+miRx923@users.noreply.github.com>
2023-05-09 23:26:16 +0200

    Adding Naive Bayes Classifier implementation

---
+++ NaiveBayesClassifier/NaiveBayesClassifier/NaiveBayesClassifier.cs
@@ -0,0 +1,117 @@
+﻿/**
+ * Class for implementing Naive Bayes Classifier
+ */
+
+namespace NaiveBayesClassifier
+{
+    
+    public class NaiveBayesClassifier
+    {
+        private Dictionary<string, int> classCounts; // how many times a class is in dataset
+        private Dictionary<string, Dictionary<string, int>> featureCounts; // how many times a feature is in a class
+        private double numOfExamples; // number of examples in training set
+           
+
+        public NaiveBayesClassifier()
+        {
+            classCounts = new Dictionary<string, int>();
+            featureCounts = new Dictionary<string, Dictionary<string, int>>();
+            numOfExamples = 0; 
+        }
+
+
+        /// <summary>
+        /// Trains the classifier using the features of the example and the class to which the example belongs.
+        /// </summary>
+        /// <param name="features">An array of strings representing features for training the classifier.</param>
+        /// <param name="label">String representing the class to which the example belongs.</param>
+        public void Train(string[] features, string label)
+        {
+            // Catching the errors
+            if (string.IsNullOrEmpty(label))
+            {
+                throw new ArgumentException("Label cannot be null or empty.");
+            }
+
+            if (features == null || features.Length == 0)
+            {
+                throw new ArgumentNullException("Features list cannot be empty.");
+            }
+
+            // Training
+            if (!classCounts.ContainsKey(label))
+            {
+                classCounts[label] = 0;
+            }
+
+            classCounts[label]++;
+
+            foreach (string feature in features) 
+            {
+                if (!featureCounts.ContainsKey(label))
+                {
+                    featureCounts[label] = new Dictionary<string, int>();
+                }
+
+                if (!featureCounts[label].ContainsKey(feature))
+                {
+                    featureCounts[label][feature] = 0;
+                }
+                featureCounts[label][feature]++;
+            }
+
+            numOfExamples++;
+        }
+
+
+        /// <summary>
+        /// Predicts the example's class based on the example's properties.
+        /// </summary>
+        /// <param name="features">Array of strings representing features of the example.</param>
+        /// <returns>String class into which the example was classified based on the highest probability.</returns>
+        public string Predict(string[] features)
+        {
+            // Catching the errors
+            if (features == null || features.Length == 0)
+            {
+                throw new ArgumentNullException("Features list cannot be empty.");
+            }
+
+            // Predicting
+            string bestLabel = "";
+            double bestScore = double.MinValue;
+
+            foreach (var _class in classCounts)
+            {
+                string label = _class.Key;
+                double classCountInDataset = _class.Value;
+
+                /*
+                 probability of class in a training set:
+                 ->    P(trieda) 
+                */
+                double score = classCountInDataset / numOfExamples;
+
+                foreach (string feature in features)
+                {
+                    if (featureCounts.ContainsKey(label) && featureCounts[label].ContainsKey(feature))
+                    {
+                        /*
+                         probability of class in a training set sums up with probabilities of features for class:
+                         ->    P(trieda) + ∑ P(atribút_i  | trieda) )
+                        */
+                        score += featureCounts[label][feature] / classCountInDataset;
+                    }
+                }
+
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestLabel = label;
+                }
+            }
+
+            return bestLabel;
+        }
+    }
+}
---
+++ NaiveBayesClassifier/NaiveBayesClassifier/Program.cs
@@ -0,0 +1,197 @@
+﻿/**
+ * Naive Bayes Classifier
+ * @author: miRx923
+ * @year: 2023
+ */
+
+
+namespace NaiveBayesClassifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * Training with Play-Tennis dataset
+             */
+            NaiveBayesClassifier classifier1 = new NaiveBayesClassifier();
+
+            using (var reader = new StreamReader("play_tennis-TrainingData.csv"))
+            {
+
+                string line = reader.ReadLine(); // skips first line
+
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    string label = values[values.Length - 1]; // saves the class label to variable
+
+                    classifier1.Train(new string[] { values[1], values[2], values[3], values[4] }, label);
+                }
+            }
+
+
+            /**
+             * Testing with Play-Tennis dataset
+             */
+
+            double truePositive = 0, falsePositive = 0, trueNegative = 0, falseNegative = 0;
+
+            using (var reader = new StreamReader("play_tennis-TestingData.csv"))
+            {
+                string line = reader.ReadLine(); // skips first line
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    string actualLabel = values[values.Length - 1];
+
+                    string[] features = new string[] { values[1], values[2], values[3], values[4] };
+                    string predictedLabel = classifier1.Predict(features);
+
+                    if (predictedLabel == "Yes" && actualLabel == "Yes")
+                    {
+                        truePositive++;
+                    }
+                    else if (predictedLabel == "Yes" && actualLabel == "No")
+                    {
+                        falsePositive++;
+                    }
+                    else if (predictedLabel == "No" && actualLabel == "No")
+                    {
+                        trueNegative++;
+                    }
+                    else if (predictedLabel == "No" && actualLabel == "Yes")
+                    {
+                        falseNegative++;
+                    }
+                }
+            }
+
+            // Efectiveness metrics
+            double precision = truePositive / (truePositive + falsePositive);
+            double recall = truePositive / (truePositive + falseNegative);
+            double f1 = 2 * precision * recall / (precision + recall);
+            double accuracy = (truePositive + trueNegative) / (truePositive + trueNegative + falsePositive + falseNegative);
+
+            // Others efectiveness metrics
+            double errorRate = (falsePositive + falseNegative) / (truePositive + trueNegative + falsePositive + falseNegative);
+            double TPR = truePositive / (truePositive + falseNegative);
+            double TNR = trueNegative / (trueNegative + falsePositive);
+            double PPV = truePositive / (truePositive + falsePositive);
+            double NPV = trueNegative / (trueNegative + falseNegative);
+            double FNR = falseNegative / (falseNegative + truePositive);
+            double FPR = falsePositive / (falsePositive + trueNegative);
+
+            Console.WriteLine(  $"Testing with Play-Tennis dataset: \n" +
+                                $"Precision: {precision} \n" +
+                                $"Recall: {recall} \n" +
+                                $"F1: {f1} \n" +
+                                $"Accuracy: {accuracy} \n\n" +
+                                $"Other efectiveness metrics: \n" +
+                                $"ErrorRate: {errorRate}   \n" +
+                                $"TruePositiveRate(TPR): {TPR}  \n" +
+                                $"TrueNegativeRate(TNR): {TNR}  \n" +
+                                $"PositivePredictiveValue(PPV): {PPV}  \n" +
+                                $"NegativePredictiveValue(NPV): {NPV}  \n" +
+                                $"TruePositiveRate(FNR): {FNR} \n" +
+                                $"FalsePositiveRate(FPR): {FPR} \n\n");
+
+
+
+
+
+            /**
+             * Training with Iris-Species dataset
+             */
+            
+            NaiveBayesClassifier classifier2 = new NaiveBayesClassifier();
+
+            using (var reader = new StreamReader("Iris-TrainingData.csv"))
+            {
+                string line = reader.ReadLine(); // skips first line
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(','); 
+                    string label = values[values.Length - 1]; // saves class label to variable
+
+                    classifier2.Train(new string[] { values[1], values[2], values[3], values[4] }, label);
+                }
+            }
+            
+
+            /**
+             * Testing with Iris-Species dataset
+             */
+
+            
+            truePositive = 0;
+            trueNegative = 0;
+            falsePositive = 0; 
+            falseNegative = 0;
+
+            using (var reader = new StreamReader("Iris-TestingData.csv"))
+            {
+                string line = reader.ReadLine(); // skips first line
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    string actualLabel = values[values.Length - 1]; // saves class label to variable
+
+                    string[] features = new string[] { values[1], values[2], values[3], values[4] };
+                    string predictedLabel = classifier2.Predict(features);
+
+                    if (predictedLabel == "Iris-setosa" && actualLabel == "Iris-setosa")
+                    {
+                        truePositive++;
+                    }
+                    else if (predictedLabel == "Iris-setosa" && actualLabel == "Iris-virginica")
+                    {
+                        falsePositive++;
+                    }
+                    else if (predictedLabel == "Iris-virginica" && actualLabel == "Iris-virginica")
+                    {
+                        trueNegative++;
+                    }
+                    else if (predictedLabel == "Iris-virginica" && actualLabel == "Iris-setosa")
+                    {
+                        falseNegative++;
+                    }
+                }
+            }
+
+            
+            // Efectiveness metrics
+            precision = truePositive / (truePositive + falsePositive);
+            recall = truePositive / (truePositive + falseNegative);
+            f1 = 2 * precision * recall / (precision + recall);
+            accuracy = (truePositive + trueNegative) / (truePositive + trueNegative + falsePositive + falseNegative);
+
+            // Other efectiveness metrics
+            errorRate = (falsePositive + falseNegative) / (truePositive + trueNegative + falsePositive + falseNegative);
+            TPR = truePositive / (truePositive + falseNegative);
+            TNR = trueNegative / (trueNegative + falsePositive);
+            PPV = truePositive / (truePositive + falsePositive);
+            NPV = trueNegative / (trueNegative + falseNegative);
+            FNR = falseNegative / (falseNegative + truePositive);
+            FPR = falsePositive / (falsePositive + trueNegative);
+
+
+            Console.WriteLine($"Testing with Iris-Species dataset: \n" +
+                                $"Precision: {precision} \n" +
+                                $"Recall: {recall} \n" +
+                                $"F1: {f1} \n" +
+                                $"Accuracy: {accuracy} \n\n" +
+                                $"Other efectiveness metrics: \n" +
+                                $"ErrorRate: {errorRate}   \n" +
+                                $"TruePositiveRate(TPR): {TPR}  \n" +
+                                $"TrueNegativeRate(TNR): {TNR}  \n" +
+                                $"PositivePredictiveValue(PPV): {PPV}  \n" +
+                                $"NegativePredictiveValue(NPV): {NPV}  \n" +
+                                $"TruePositiveRate(FNR): {FNR} \n" +
+                                $"FalsePositiveRate(FPR): {FPR} \n\n");
+        }
+    }
+}
